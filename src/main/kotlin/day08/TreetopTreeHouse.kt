22c6163c428407f77b8d6afafcdefeb06e766093
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-12-11 23:47:36 +0100

    feature(day08): failure attempt part2

--- src/main/kotlin/day08/TreetopTreeHouse.kt
+++ src/main/kotlin/day08/TreetopTreeHouse.kt
@@ -18,6 +18,34 @@ fun main() {
   fun isVisibleFromDown(matrix: List<List<Int>>, rowIndex: Int, columnIndex: Int, elem: Int) =
     matrix.drop(rowIndex + 1).map { it[columnIndex] }.all { it < elem }
 
+  fun viewDistanceRight(matrix: List<List<Int>>, rowIndex: Int, columnIndex: Int): Int {
+    val trees = matrix[rowIndex].drop(columnIndex + 1)
+    val windows = trees.windowed(2, 1)
+    val count = windows.takeWhile { it[0] < it[1] }.count()
+    return if (windows.isNotEmpty()) count + 1 else count
+  }
+
+  fun viewDistanceLeft(matrix: List<List<Int>>, rowIndex: Int, columnIndex: Int): Int {
+    val trees = matrix[rowIndex].take(columnIndex).reversed()
+    val windows = trees.windowed(2, 1)
+    val count = windows.takeWhile { it[0] < it[1] }.count()
+    return if (windows.isNotEmpty()) count + 1 else count
+  }
+
+  fun viewDistanceUp(matrix: List<List<Int>>, rowIndex: Int, columnIndex: Int): Int {
+    val trees = matrix.take(rowIndex).map { it[columnIndex] }.reversed()
+    val windows = trees.windowed(2, 1)
+    val count = windows.takeWhile { it[0] < it[1] }.count()
+    return if (windows.isNotEmpty()) count + 1 else count
+  }
+
+  fun viewDistanceDown(matrix: List<List<Int>>, rowIndex: Int, columnIndex: Int): Int {
+    val trees = matrix.drop(rowIndex + 1).map { it[columnIndex] }
+    val windows = trees.windowed(2, 1)
+    val count = windows.takeWhile { it[0] < it[1] }.count()
+    return if (windows.isNotEmpty()) count + 1 else count
+  }
+
   fun List<List<Int>>.toVisibilityMap() = mapIndexed { rowIndex, row ->
     row.mapIndexed { columnIndex, elem ->
       isVisibleFromRight(this, rowIndex, columnIndex, elem)
@@ -27,10 +55,21 @@ fun main() {
     }
   }
 
+  fun List<List<Int>>.toScenicScoreMap() = mapIndexed { rowIndex, row ->
+    List(row.size) { columnIndex ->
+      viewDistanceRight(this, rowIndex, columnIndex) *
+          viewDistanceLeft(this, rowIndex, columnIndex) *
+          viewDistanceUp(this, rowIndex, columnIndex) *
+          viewDistanceDown(this, rowIndex, columnIndex)
+    }
+  }
+
   fun partOne(input: String) = input.createMatrix().toVisibilityMap().flatten().count { it }
+
+  fun partTwo(input: String) = input.createMatrix().toScenicScoreMap()//.flatten().maxOf { it }
 
   val input = readFile("/day08/input.txt")
   println(partOne(input))
-  //println(partTwo(input))
+  println(partTwo(input))
 
 }
